173f44cf27891afde3094fa0026007f08a0bd091
Shahriar Rahman <shahriar.rahman10@northsouth.edu>
2023-06-22 07:54:17 +0600

    License Updated

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1.0',
     description='This is an automated Web scraping project using Selenium to access all the books in Audible.',
     author='Shahriar',
-    license='',
+    license='MIT License',
 )
